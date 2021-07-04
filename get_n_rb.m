525219c1010f94922e4258911b21b4388956d7cd
Adam Księżyk <adam.ksiezyk@gmial.com>
2021-07-05 00:21:25 +0200

    Fix BW in get_n_rb.m

--- get_n_rb.m
+++ get_n_rb.m
@@ -10,69 +10,69 @@ function N_RB = get_n_rb(scs, bw)
     if (scs == 15e3)
         if (bw == 5e6)
             N_RB = 25;
-        elseif (bw == 10e3)
+        elseif (bw == 10e6)
             N_RB = 52;
-        elseif (bw == 15e3)
+        elseif (bw == 15e6)
             N_RB = 79;
-        elseif (bw == 20e3)
+        elseif (bw == 20e6)
             N_RB = 106;
-        elseif (bw == 25e3)
+        elseif (bw == 25e6)
             N_RB = 133;
-        elseif (bw == 30e3)
+        elseif (bw == 30e6)
             N_RB = 160;
-        elseif (bw == 40e3)
+        elseif (bw == 40e6)
             N_RB = 216;
-        elseif (bw == 50e3)
+        elseif (bw == 50e6)
             N_RB = 270;
         end
     elseif (scs == 30e3)
         if (bw == 5e6)
             N_RB = 11;
-        elseif (bw == 10e3)
+        elseif (bw == 10e6)
             N_RB = 24;
-        elseif (bw == 15e3)
+        elseif (bw == 15e6)
             N_RB = 38;
-        elseif (bw == 20e3)
+        elseif (bw == 20e6)
             N_RB = 51;
-        elseif (bw == 25e3)
+        elseif (bw == 25e6)
             N_RB = 65;
-        elseif (bw == 30e3)
+        elseif (bw == 30e6)
             N_RB = 78;
-        elseif (bw == 40e3)
+        elseif (bw == 40e6)
             N_RB = 106;
-        elseif (bw == 50e3)
+        elseif (bw == 50e6)
             N_RB = 133;
-        elseif (bw == 60e3)
+        elseif (bw == 60e6)
             N_RB = 162;
-        elseif (bw == 80e3)
+        elseif (bw == 80e6)
             N_RB = 217;
-        elseif (bw == 90e3)
+        elseif (bw == 90e6)
             N_RB = 245;
-        elseif (bw == 100e3)
+        elseif (bw == 100e6)
             N_RB = 273;
         end
     elseif (scs == 60e3)
-        if (bw == 10e3)
+        if (bw == 10e6)
             N_RB = 11;
-        elseif (bw == 15e3)
+        elseif (bw == 15e6)
             N_RB = 18;
-        elseif (bw == 20e3)
+        elseif (bw == 20e6)
             N_RB = 24;
-        elseif (bw == 25e3)
+        elseif (bw == 25e6)
             N_RB = 31;
-        elseif (bw == 30e3)
+        elseif (bw == 30e6)
             N_RB = 38;
-        elseif (bw == 40e3)
+        elseif (bw == 40e6)
             N_RB = 51;
-        elseif (bw == 50e3)
+        elseif (bw == 50e6)
             N_RB = 65;
-        elseif (bw == 60e3)
+        elseif (bw == 60e6)
             N_RB = 79;
-        elseif (bw == 80e3)
+        elseif (bw == 80e6)
             N_RB = 107;
-        elseif (bw == 90e3)
+        elseif (bw == 90e6)
             N_RB = 121;
-        elseif (bw == 100e3)
+        elseif (bw == 100e6)
             N_RB = 135;
         end
     end
