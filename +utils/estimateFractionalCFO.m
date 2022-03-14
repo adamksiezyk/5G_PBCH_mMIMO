59865a0fa55f7275fb99bde9c0c2f5d8de712257
Adam Księżyk <adam.ksiezyk@gmial.com>
2022-03-14 23:18:37 +0100

    Change fractional CFO estimation method

--- +utils/estimateFractionalCFO.m
+++ +utils/estimateFractionalCFO.m
@@ -1,5 +1,5 @@
-function [fractional_CFO, CP_corr] = estimateFractionalCFO(waveform, ...
-    sample_rate, scs, ssb_frequency_offset)
+function [fractional_CFO, CP_corr, indv_CFO] = estimateFractionalCFO(waveform, ...
+    sample_rate, scs)
 %ESTIMATEFRACTIONALCFO Estimated the fractional center frequency offset
 % Inputs:
 %   waveform                : a column vector representing the signal
@@ -13,41 +13,44 @@ function [fractional_CFO, CP_corr] = estimateFractionalCFO(waveform, ...
 %   frequency offset
 %   CP_corr         : a vector representing the calculated cyclic prefix
 %   based correlation
+%   indv_CFO        : a vector representing the individual fractional CFO
+%   values
 
-    % Shift the SSB to center frequency
-    t = (0:length(waveform)-1) / sample_rate;
-    waveform_center = waveform .* exp(-1i*2*pi*ssb_frequency_offset*t);
-    
-    % Downsample
-    N_FFT_ds = 256;
-    sample_rate_ds = N_FFT_ds * scs;
-    [~, N_CP_ds] = utils.getCPLength(scs, sample_rate_ds);
-    N_sym_ds = N_CP_ds + N_FFT_ds;
-    waveform_ds = resample(waveform_center, sample_rate_ds, sample_rate);
-    
-    % Find PSS time indices
-    pss_indices = PSS.detectAndDecodePSS(waveform_ds, N_FFT_ds, ...
-        N_CP_ds, 0, 0, false);
-    time_offset = pss_indices(1);
-    waveform_shift = waveform_ds(time_offset:end);
+    % Signal autocorelation
+    N_CP = utils.getCPLength(scs, sample_rate);
+    N_FFT = 2048;
+    N_sym = N_FFT + N_CP;
+    N_corr = sample_rate * 10e-3;   % 1 5G frame
+    CP_corr = zeros(1, N_corr);
+    for n = 1:N_corr
+        n1 = n:n+N_CP-1; 
+        n2 = n1 + N_FFT;
+        CP_corr(n) = abs(sum( waveform(n1) .* conj(waveform(n2))));
+    end
 
-    % Multiply the waveform by itself delayed by N_sym samples and conjugated
-    delayed = [zeros(1, N_FFT_ds), waveform_shift(1:end-N_FFT_ds)];
-    cpProduct = waveform_shift .* conj(delayed);
-
-    % Apply a moving sum filter with a window size equal to the CP length
-    CP_corr = filter(ones([N_CP_ds 1]), 1, cpProduct);
-
-    % Moving sum over 4 OFDM symbols (SSB size)
-    CP_corr_delayed = CP_corr;
-    for k = 1:3
-        CP_corr_delayed = [zeros(1, N_sym_ds), ...
-            CP_corr_delayed(1:end-N_sym_ds)];
-        CP_corr = CP_corr + CP_corr_delayed;
+    %find a few max peaks
+    CP_corr_copy = CP_corr;
+    npeak = 50;
+    CP_pos = zeros(1, npeak);
+    for n = 1:npeak
+        [~, CP_pos(n)] = max(CP_corr_copy);
+        %don't take indexes from beyond array length
+        start = max(1, CP_pos(n)-N_sym);
+        stop = min(length(CP_corr_copy), CP_pos(n)+N_sym);
+        %clean this symbol to ensure that we don't take it again
+        CP_corr_copy(start:stop) = zeros(1, stop-start+1);
     end
     
-    % Estimate frequency offset
-    CP_corr_idx = N_FFT_ds + N_CP_ds + 3*N_sym_ds; % 4*N_sym = N_FFT (delayed correlation) + N_CP (sum filter CP) + 3*N_sym (moving sum filter of 4 symbols)
-    fractional_CFO =  scs * angle(mean(CP_corr(CP_corr_idx))) / (2*pi);
+    %calculate CFO for choosen symbols
+    CP_pos = sort(CP_pos);
+    indv_CFO = zeros(1, npeak);
+    for n = 1:npeak 
+        n1 = CP_pos(n):CP_pos(n)+N_CP-1;
+        n2 = n1 + N_FFT;
+        indv_CFO(n) = sample_rate/N_FFT * mean(angle(conj(waveform(n1)) .* waveform(n2)) / (2*pi));
+    end
+    
+    %correct signal with mean of different CFO values
+    fractional_CFO = mean(indv_CFO);
 end
 
